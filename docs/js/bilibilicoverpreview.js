e71d86f3c277b4cb4ff34eb5775fa97d78f826c0
wby <walkedby@icloud.com>
2020-02-12 15:01:58 +0800

    2020-02-12 15:01:58

--- docs/js/bilibilicoverpreview.js
+++ docs/js/bilibilicoverpreview.js
@@ -1,1 +1,83 @@
-"use strict";var uploadCover=new Image();uploadCover.onload=RefreshPreview;var appbg=new Image();appbg.src="/images/bilibili_cover_preview_images/app.png";var appsubbg=new Image();appsubbg.src="/images/bilibili_cover_preview_images/appsub.png";var websubbg=new Image();websubbg.src="/images/bilibili_cover_preview_images/websub.png";var websub2bg=new Image();websub2bg.src="/images/bilibili_cover_preview_images/websub2.png";var webspacebg=new Image();webspacebg.src="/images/bilibili_cover_preview_images/webspace.png";(function(){var h=document.getElementById("uploadimg");if(h==null){throw"uploadimg is null";}var input=h;input.onchange=function(){if(input.files!=null){var uploadimg=input.files[0];var reader=new FileReader;reader.readAsDataURL(uploadimg);reader.onload=function(ev){if(this.result){uploadCover.src=this.result.toString()}}}};document.getElementsByName("bgtype").forEach(function(e){var ie=e;ie.onclick=RefreshPreview})})();function RefreshPreview(){if(uploadCover.src.length<1){return}var h=document.getElementById("drawing");if(h==null){throw"canvas drawing is null";}var canvas=h;var ctx=canvas.getContext("2d");if(ctx==null){throw"canvas 2d context is null";}canvas.height=canvas.height;var bgtype="app";document.getElementsByName("bgtype").forEach(function(e){var ie=e;if(ie.checked){bgtype=ie.value}});switch(bgtype){case"app":ctx.drawImage(uploadCover,10,90,183,115);ctx.drawImage(appbg,0,0);break;case"appsub":ctx.drawImage(uploadCover,14,195,370,209);ctx.drawImage(appsubbg,0,0);break;case"websub":ctx.drawImage(uploadCover,247,202,64,36);ctx.drawImage(websubbg,0,0);break;case"websub2":ctx.drawImage(uploadCover,98,115,207,130);ctx.drawImage(websub2bg,0,0);break;case"webspace":ctx.drawImage(uploadCover,229,123,190,120);ctx.drawImage(webspacebg,0,0);break;default:throw"no such bgtype:"+bgtype;break}}
+"use strict";
+var uploadCover = new Image();
+uploadCover.onload = RefreshPreview;
+var appbg = new Image();
+appbg.src = "/images/bilibili_cover_preview_images/app.png";
+var appsubbg = new Image();
+appsubbg.src = "/images/bilibili_cover_preview_images/appsub.png";
+var websubbg = new Image();
+websubbg.src = "/images/bilibili_cover_preview_images/websub.png";
+var websub2bg = new Image();
+websub2bg.src = "/images/bilibili_cover_preview_images/websub2.png";
+var webspacebg = new Image();
+webspacebg.src = "/images/bilibili_cover_preview_images/webspace.png";
+(function () {
+    var h = document.getElementById("uploadimg");
+    if (h == null) {
+        throw "uploadimg is null";
+    }
+    var input = h;
+    input.onchange = function () {
+        if (input.files != null) {
+            var uploadimg = input.files[0];
+            var reader = new FileReader;
+            reader.readAsDataURL(uploadimg);
+            reader.onload = function (ev) {
+                if (this.result) {
+                    uploadCover.src = this.result.toString();
+                }
+            };
+        }
+    };
+    document.getElementsByName("bgtype").forEach(function (e) {
+        var ie = e;
+        ie.onclick = RefreshPreview;
+    });
+})();
+function RefreshPreview() {
+    if (uploadCover.src.length < 1) {
+        return;
+    }
+    var h = document.getElementById("drawing");
+    if (h == null) {
+        throw "canvas drawing is null";
+    }
+    var canvas = h;
+    var ctx = canvas.getContext("2d");
+    if (ctx == null) {
+        throw "canvas 2d context is null";
+    }
+    canvas.height = canvas.height;
+    var bgtype = "app";
+    document.getElementsByName("bgtype").forEach(function (e) {
+        var ie = e;
+        if (ie.checked) {
+            bgtype = ie.value;
+        }
+    });
+    switch (bgtype) {
+        case "app":
+            ctx.drawImage(uploadCover, 10, 90, 183, 115);
+            ctx.drawImage(appbg, 0, 0);
+            break;
+        case "appsub":
+            ctx.drawImage(uploadCover, 14, 183, 370, 231);
+            ctx.drawImage(appsubbg, 0, 0);
+            break;
+        case "websub":
+            ctx.drawImage(uploadCover, 247, 201, 64, 40);
+            ctx.drawImage(websubbg, 0, 0);
+            break;
+        case "websub2":
+            ctx.drawImage(uploadCover, 98, 115, 207, 130);
+            ctx.drawImage(websub2bg, 0, 0);
+            break;
+        case "webspace":
+            ctx.drawImage(uploadCover, 229, 123, 190, 120);
+            ctx.drawImage(webspacebg, 0, 0);
+            break;
+        default:
+            throw "no such bgtype:" + bgtype;
+            break;
+    }
+}
--- source/_posts/2020/02/bilibilicoverpreview.md
+++ source/_posts/2020/02/bilibilicoverpreview.md
@@ -4,7 +4,8 @@ date: 2020-02-12 09:27:54
 tags: [我的软件,视频创作]
 ---
 本工具提供主要几种情况下B站投稿封面的效果预览。  
-B站视频封面必须是 jpg 或 png 格式的图片，文件不大于 5MB，封面比例是 16:10 ，建议尺寸是 1146x717，最低尺寸是 960x600，恕不提供裁剪的功能。
+本工具适合在电脑浏览器上使用，预览效果仅供参考，以B站实际展示为准。  
+B站视频封面必须是 jpg 或 png 格式的图片，文件不大于 5MB，封面比例是 16:10 ，建议尺寸是 1146x717，最低尺寸是 960x600，恕不提供裁剪的功能。  
 
 <div>
     选择你的封面文件：<input type="file" accept="image/jpeg, image/jpg, image/png" id="uploadimg">
@@ -12,6 +13,7 @@ B站视频封面必须是 jpg 或 png 格式的图片，文件不大于 5MB，�
     <input type="radio" name="bgtype" value="app" checked="checked">APP首页推荐
     <input type="radio" name="bgtype" value="appsub">APP订阅页面
     <input type="radio" name="bgtype" value="websub">网页动态
+    <br>
     <input type="radio" name="bgtype" value="websub2">网页动态2
     <input type="radio" name="bgtype" value="webspace">网页个人空间
     <br><br>
